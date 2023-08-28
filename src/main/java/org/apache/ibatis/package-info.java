2396608aca999746c64c5add40c1317f06a859b2
JlU-YOU <102222347+JlU-YOU@users.noreply.github.com>
2023-08-28 19:25:23 +0800

    格式化

--- src/main/java/org/apache/ibatis/package-info.java
+++ src/main/java/org/apache/ibatis/package-info.java
@@ -1,17 +1,21 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * The MyBatis data mapper framework makes it easier to use a relational database with object-oriented applications.
+ * <p>
+ * package-info.java 文件为Java包提供了一种集中管理包级别元数据和注释的方式，有助于提供更清晰的代码文档、更好的代码组织以及一些额外的语义信息。虽然这个文件不是必需的，但在需要为整个包添加注释、元数据或其他信息时，它是一个很有用的工具。
  */
 /**
  * The MyBatis data mapper framework makes it easier to use a relational database with object-oriented applications.
