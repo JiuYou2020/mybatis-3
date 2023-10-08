ea3c431c34c5df0a07c8e22cc90deb4cd5cd2df5
JlU-YOU <102222347+JlU-YOU@users.noreply.github.com>
2023-10-08 18:01:44 +0800

    反射

---
+++ src/test/java/org/apache/ibatis/reflection/InvokerTest.java
@@ -0,0 +1,9 @@
+package org.apache.ibatis.reflection;/**
+ * {@code @Author: } JiuYou2020
+ * <br>
+ * {@code @Date: } 2023/10/7
+ * <br>
+ * {@code @Description: }
+ */
+public class InvokerTest {
+}
---
+++ src/test/java/org/apache/ibatis/reflection/PropertyTest.java
@@ -0,0 +1,9 @@
+package org.apache.ibatis.reflection;/**
+ * {@code @Author: } JiuYou2020
+ * <br>
+ * {@code @Date: } 2023/10/8
+ * <br>
+ * {@code @Description: }
+ */
+public class PropertyTest {
+}
